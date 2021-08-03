6ca4d2893ba8807f070982ef8f6fcb78411a79ac
Juan Pablo Castano <jpcastanom@gmail.com>
2021-08-03 16:58:41 -0500

    all iterations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+// import logo from "./logo.svg"
+import "./App.css"
+import contacts from "./contacts.json"
+import { useState } from "react"
 
 function App() {
+  const [contactList, setContactList] = useState(contacts.slice(0, 5))
+
+  const addRandomContact = (e) => {
+    e.preventDefault()
+
+    const filteredArray = contacts.filter((contact) => {
+      return !contactList.find((existingContact) => {
+        return existingContact.id === contact.id
+      })
+    })
+
+    const randomIndex = Math.floor(Math.random() * filteredArray.length)
+    console.log("randomIndex", randomIndex)
+    const selectedContact = filteredArray[randomIndex]
+
+    console.log(selectedContact)
+    console.log(contactList.length)
+    console.log(filteredArray.length)
+
+    setContactList([
+      ...contactList,
+      {
+        name: selectedContact.name,
+        pictureUrl: selectedContact.pictureUrl,
+        popularity: selectedContact.popularity,
+        id: selectedContact.id,
+      },
+    ])
+  }
+
+  const sortByPopularity = (e) => {
+    e.preventDefault()
+    const sortedArray = [...contactList].sort((a, b) => {
+      return a.popularity - b.popularity
+    })
+
+    setContactList(sortedArray)
+  }
+
+  const sortByName = (e) => {
+    e.preventDefault()
+    const sortedArray = [...contactList].sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
+
+    setContactList(sortedArray)
+  }
+
+  const deleteContact = (event, element) => {
+    event.preventDefault()
+
+    const filteredArray = contactList.filter((contact) => {
+      return contact.id !== element.id
+    })
+
+    setContactList(filteredArray)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="content">
+        <h1>IronContacts</h1>
+        <button
+          onClick={(e) => {
+            addRandomContact(e)
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add Random Contact
+        </button>
+        <button
+          onClick={(e) => {
+            sortByName(e)
+          }}
+        >
+          Sort by name
+        </button>
+        <button
+          onClick={(e) => {
+            sortByPopularity(e)
+          }}
+        >
+          Sort by popularity
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contactList.map((element) => {
+              return (
+                <tr>
+                  <td>
+                    <img
+                      src={element.pictureUrl}
+                      alt="contact-picture"
+                      className="contactImg"
+                    ></img>
+                  </td>
+                  <td>{element.name}</td>
+                  <td>{element.popularity.toFixed(2)}</td>
+                  <td>
+                    <button
+                      onClick={(event) => {
+                        deleteContact(event, element)
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
